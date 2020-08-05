b690e12749764f2116d8ea6c92b2f1b590aa6b77
Frank Dellaert <dellaert@gmail.com>
2020-08-05 11:28:46 -0400

    Switched to Shonan Content

--- src/entry.jsx
+++ src/entry.jsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import Fancy from './components/Fancy'
 import PivotTable from './components/PivotTable'
 
-ReactDOM.render(<Fancy />, document.getElementById('fancy'));
+// ReactDOM.render(<Fancy />, document.getElementById('fancy'));
 ReactDOM.render(<PivotTable />, document.getElementById('pivot'));
