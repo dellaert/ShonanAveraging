ae496b7918606fdf9c7b440320ef01ecddd8abce
Frank Dellaert <dellaert@gmail.com>
2020-08-09 19:56:08 -0400

    Added read_g2o

--- ceres/pose_graph_2d.cc
+++ ceres/pose_graph_2d.cc
@@ -7,6 +7,7 @@
 #include "ceres/ceres.h"
 #include "gflags/gflags.h"
 #include "glog/logging.h"
+#include "read_g2o.h"
 
 DEFINE_string(input, "", "The pose graph definition filename in g2o format.");
 
@@ -21,21 +22,18 @@ struct Pose2d {
   double yaw_radians;
 
   // The name of the data type in the g2o file format.
-  static std::string name() {
-    return "VERTEX_SE2";
-  }
+  static std::string name() { return "VERTEX_SE2"; }
 };
 
 // Normalizes the angle in radians between [-pi and pi).
-template <typename T>
-inline T NormalizeAngle(const T& angle_radians) {
+template <typename T> inline T NormalizeAngle(const T &angle_radians) {
   // Use ceres::floor because it is specialized for double and Jet types.
   T two_pi(2.0 * M_PI);
   return angle_radians -
-      two_pi * ceres::floor((angle_radians + T(M_PI)) / two_pi);
+         two_pi * ceres::floor((angle_radians + T(M_PI)) / two_pi);
 }
 
-inline std::istream& operator>>(std::istream& input, Pose2d& pose) {
+inline std::istream &operator>>(std::istream &input, Pose2d &pose) {
   input >> pose.x >> pose.y >> pose.yaw_radians;
   // Normalize the angle between -pi to pi.
   pose.yaw_radians = NormalizeAngle(pose.yaw_radians);
@@ -57,17 +55,15 @@ struct Constraint2d {
   Eigen::Matrix3d information;
 
   // The name of the data type in the g2o file format.
-  static std::string name() {
-    return "EDGE_SE2";
-  }
+  static std::string name() { return "EDGE_SE2"; }
 };
 
-inline std::istream& operator>>(std::istream& input, Constraint2d& constraint) {
+inline std::istream &operator>>(std::istream &input, Constraint2d &constraint) {
   input >> constraint.id_begin >> constraint.id_end >> constraint.x >>
-      constraint.y >> constraint.yaw_radians >>
-      constraint.information(0, 0) >> constraint.information(0, 1) >>
-      constraint.information(0, 2) >> constraint.information(1, 1) >>
-      constraint.information(1, 2) >> constraint.information(2, 2);
+      constraint.y >> constraint.yaw_radians >> constraint.information(0, 0) >>
+      constraint.information(0, 1) >> constraint.information(0, 2) >>
+      constraint.information(1, 1) >> constraint.information(1, 2) >>
+      constraint.information(2, 2);
 
   // Set the lower triangular part of the information matrix.
   constraint.information(1, 0) = constraint.information(0, 1);
@@ -82,8 +78,8 @@ inline std::istream& operator>>(std::istream& input, Constraint2d& constraint) {
 // Reads a single pose from the input and inserts it into the map. Returns false
 // if there is a duplicate entry.
 template <typename Pose, typename Allocator>
-bool ReadVertex(std::ifstream* infile,
-                std::map<int, Pose, std::less<int>, Allocator>* poses) {
+bool ReadVertex(std::ifstream *infile,
+                std::map<int, Pose, std::less<int>, Allocator> *poses) {
   int id;
   Pose pose;
   *infile >> id >> pose;
@@ -100,81 +96,15 @@ bool ReadVertex(std::ifstream* infile,
 
 // Reads the contraints between two vertices in the pose graph
 template <typename Constraint, typename Allocator>
-void ReadConstraint(std::ifstream* infile,
-                    std::vector<Constraint, Allocator>* constraints) {
+void ReadConstraint(std::ifstream *infile,
+                    std::vector<Constraint, Allocator> *constraints) {
   Constraint constraint;
   *infile >> constraint;
 
   constraints->push_back(constraint);
 }
 
-// Reads a file in the g2o filename format that describes a pose graph
-// problem. The g2o format consists of two entries, vertices and constraints.
-//
-// In 2D, a vertex is defined as follows:
-//
-// VERTEX_SE2 ID x_meters y_meters yaw_radians
-//
-// A constraint is defined as follows:
-//
-// EDGE_SE2 ID_A ID_B A_x_B A_y_B A_yaw_B I_11 I_12 I_13 I_22 I_23 I_33
-//
-// where I_ij is the (i, j)-th entry of the information matrix for the
-// measurement.
-//
-//
-// In 3D, a vertex is defined as follows:
-//
-// VERTEX_SE3:QUAT ID x y z q_x q_y q_z q_w
-//
-// where the quaternion is in Hamilton form.
-// A constraint is defined as follows:
-//
-// EDGE_SE3:QUAT ID_a ID_b x_ab y_ab z_ab q_x_ab q_y_ab q_z_ab q_w_ab I_11 I_12 I_13 ... I_16 I_22 I_23 ... I_26 ... I_66 // NOLINT
-//
-// where I_ij is the (i, j)-th entry of the information matrix for the
-// measurement. Only the upper-triangular part is stored. The measurement order
-// is the delta position followed by the delta orientation.
-template <typename Pose, typename Constraint, typename MapAllocator,
-          typename VectorAllocator>
-bool ReadG2oFile(const std::string& filename,
-                 std::map<int, Pose, std::less<int>, MapAllocator>* poses,
-                 std::vector<Constraint, VectorAllocator>* constraints) {
-  CHECK(poses != NULL);
-  CHECK(constraints != NULL);
-
-  poses->clear();
-  constraints->clear();
-
-  std::ifstream infile(filename.c_str());
-  if (!infile) {
-    return false;
-  }
-
-  std::string data_type;
-  while (infile.good()) {
-    // Read whether the type is a node or a constraint.
-    infile >> data_type;
-    if (data_type == Pose::name()) {
-      if (!ReadVertex(&infile, poses)) {
-        return false;
-      }
-    } else if (data_type == Constraint::name()) {
-      ReadConstraint(&infile, constraints);
-    } else {
-      LOG(ERROR) << "Unknown data type: " << data_type;
-      return false;
-    }
-
-    // Clear any trailing whitespace from the line.
-    infile >> std::ws;
-  }
-
-  return true;
-}
-
-template <typename T>
-Eigen::Matrix<T, 2, 2> RotationMatrix2D(T yaw_radians) {
+template <typename T> Eigen::Matrix<T, 2, 2> RotationMatrix2D(T yaw_radians) {
   const T cos_yaw = ceres::cos(yaw_radians);
   const T sin_yaw = ceres::sin(yaw_radians);
 
@@ -193,25 +123,23 @@ Eigen::Matrix<T, 2, 2> RotationMatrix2D(T yaw_radians) {
 // into the global frame, and Normalize(*) ensures the angles are in the range
 // [-pi, pi).
 class PoseGraph2dErrorTerm {
- public:
+public:
   PoseGraph2dErrorTerm(double x_ab, double y_ab, double yaw_ab_radians,
-                       const Eigen::Matrix3d& sqrt_information)
-      : p_ab_(x_ab, y_ab),
-        yaw_ab_radians_(yaw_ab_radians),
+                       const Eigen::Matrix3d &sqrt_information)
+      : p_ab_(x_ab, y_ab), yaw_ab_radians_(yaw_ab_radians),
         sqrt_information_(sqrt_information) {}
 
   template <typename T>
-  bool operator()(const T* const x_a, const T* const y_a, const T* const yaw_a,
-                  const T* const x_b, const T* const y_b, const T* const yaw_b,
-                  T* residuals_ptr) const {
+  bool operator()(const T *const x_a, const T *const y_a, const T *const yaw_a,
+                  const T *const x_b, const T *const y_b, const T *const yaw_b,
+                  T *residuals_ptr) const {
     const Eigen::Matrix<T, 2, 1> p_a(*x_a, *y_a);
     const Eigen::Matrix<T, 2, 1> p_b(*x_b, *y_b);
 
-    Eigen::Map<Eigen::Matrix<T, 3, 1> > residuals_map(residuals_ptr);
+    Eigen::Map<Eigen::Matrix<T, 3, 1>> residuals_map(residuals_ptr);
 
     residuals_map.template head<2>() =
-        RotationMatrix2D(*yaw_a).transpose() * (p_b - p_a) -
-        p_ab_.cast<T>();
+        RotationMatrix2D(*yaw_a).transpose() * (p_b - p_a) - p_ab_.cast<T>();
     residuals_map(2) = ceres::examples::NormalizeAngle(
         (*yaw_b - *yaw_a) - static_cast<T>(yaw_ab_radians_));
 
@@ -222,9 +150,9 @@ class PoseGraph2dErrorTerm {
     return true;
   }
 
-  static ceres::CostFunction* Create(double x_ab, double y_ab,
+  static ceres::CostFunction *Create(double x_ab, double y_ab,
                                      double yaw_ab_radians,
-                                     const Eigen::Matrix3d& sqrt_information) {
+                                     const Eigen::Matrix3d &sqrt_information) {
     return (new ceres::AutoDiffCostFunction<PoseGraph2dErrorTerm, 3, 1, 1, 1, 1,
                                             1, 1>(new PoseGraph2dErrorTerm(
         x_ab, y_ab, yaw_ab_radians, sqrt_information)));
@@ -232,7 +160,7 @@ class PoseGraph2dErrorTerm {
 
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
- private:
+private:
   // The position of B relative to A in the A frame.
   const Eigen::Vector2d p_ab_;
   // The orientation of frame B relative to frame A.
@@ -244,18 +172,17 @@ class PoseGraph2dErrorTerm {
 // Defines a local parameterization for updating the angle to be constrained in
 // [-pi to pi).
 class AngleLocalParameterization {
- public:
-
+public:
   template <typename T>
-  bool operator()(const T* theta_radians, const T* delta_theta_radians,
-                  T* theta_radians_plus_delta) const {
+  bool operator()(const T *theta_radians, const T *delta_theta_radians,
+                  T *theta_radians_plus_delta) const {
     *theta_radians_plus_delta =
         NormalizeAngle(*theta_radians + *delta_theta_radians);
 
     return true;
   }
 
-  static ceres::LocalParameterization* Create() {
+  static ceres::LocalParameterization *Create() {
     return (new ceres::AutoDiffLocalParameterization<AngleLocalParameterization,
                                                      1, 1>);
   }
@@ -263,9 +190,9 @@ class AngleLocalParameterization {
 
 // Constructs the nonlinear least squares optimization problem from the pose
 // graph constraints.
-void BuildOptimizationProblem(const std::vector<Constraint2d>& constraints,
-                              std::map<int, Pose2d>* poses,
-                              ceres::Problem* problem) {
+void BuildOptimizationProblem(const std::vector<Constraint2d> &constraints,
+                              std::map<int, Pose2d> *poses,
+                              ceres::Problem *problem) {
   CHECK(poses != NULL);
   CHECK(problem != NULL);
   if (constraints.empty()) {
@@ -273,50 +200,42 @@ void BuildOptimizationProblem(const std::vector<Constraint2d>& constraints,
     return;
   }
 
-  ceres::LossFunction* loss_function = NULL;
-  ceres::LocalParameterization* angle_local_parameterization =
+  ceres::LossFunction *loss_function = NULL;
+  ceres::LocalParameterization *angle_local_parameterization =
       AngleLocalParameterization::Create();
 
-  for (std::vector<Constraint2d>::const_iterator constraints_iter =
-           constraints.begin();
-       constraints_iter != constraints.end(); ++constraints_iter) {
-    const Constraint2d& constraint = *constraints_iter;
-
-    std::map<int, Pose2d>::iterator pose_begin_iter =
-        poses->find(constraint.id_begin);
-    CHECK(pose_begin_iter != poses->end())
+  for (const Constraint2d &constraint : constraints) {
+    std::map<int, Pose2d>::iterator it1 = poses->find(constraint.id_begin);
+    CHECK(it1 != poses->end())
         << "Pose with ID: " << constraint.id_begin << " not found.";
-    std::map<int, Pose2d>::iterator pose_end_iter =
-        poses->find(constraint.id_end);
-    CHECK(pose_end_iter != poses->end())
+    std::map<int, Pose2d>::iterator it2 = poses->find(constraint.id_end);
+    CHECK(it2 != poses->end())
         << "Pose with ID: " << constraint.id_end << " not found.";
 
     const Eigen::Matrix3d sqrt_information =
         constraint.information.llt().matrixL();
     // Ceres will take ownership of the pointer.
-    ceres::CostFunction* cost_function = PoseGraph2dErrorTerm::Create(
+    ceres::CostFunction *cost_function = PoseGraph2dErrorTerm::Create(
         constraint.x, constraint.y, constraint.yaw_radians, sqrt_information);
-    problem->AddResidualBlock(
-        cost_function, loss_function, &pose_begin_iter->second.x,
-        &pose_begin_iter->second.y, &pose_begin_iter->second.yaw_radians,
-        &pose_end_iter->second.x, &pose_end_iter->second.y,
-        &pose_end_iter->second.yaw_radians);
+    problem->AddResidualBlock(cost_function, loss_function, &it1->second.x,
+                              &it1->second.y, &it1->second.yaw_radians,
+                              &it2->second.x, &it2->second.y,
+                              &it2->second.yaw_radians);
 
-    problem->SetParameterization(&pose_begin_iter->second.yaw_radians,
-                                angle_local_parameterization);
-    problem->SetParameterization(&pose_end_iter->second.yaw_radians,
-                                angle_local_parameterization);
+    problem->SetParameterization(&it1->second.yaw_radians,
+                                 angle_local_parameterization);
+    problem->SetParameterization(&it2->second.yaw_radians,
+                                 angle_local_parameterization);
   }
 
   // The pose graph optimization problem has three DOFs that are not fully
   // constrained. This is typically referred to as gauge freedom. You can apply
   // a rigid body transformation to all the nodes and the optimization problem
   // will still have the exact same cost. The Levenberg-Marquardt algorithm has
-  // internal damping which mitigate this issue, but it is better to properly
+  // internal damping which mitigate this issue, but it1 is better to properly
   // constrain the gauge freedom. This can be done by setting one of the poses
-  // as constant so the optimizer cannot change it.
-  std::map<int, Pose2d>::iterator pose_start_iter =
-      poses->begin();
+  // as constant so the optimizer cannot change it1.
+  std::map<int, Pose2d>::iterator pose_start_iter = poses->begin();
   CHECK(pose_start_iter != poses->end()) << "There are no poses.";
   problem->SetParameterBlockConstant(&pose_start_iter->second.x);
   problem->SetParameterBlockConstant(&pose_start_iter->second.y);
@@ -324,7 +243,7 @@ void BuildOptimizationProblem(const std::vector<Constraint2d>& constraints,
 }
 
 // Returns true if the solve was successful.
-bool SolveOptimizationProblem(ceres::Problem* problem) {
+bool SolveOptimizationProblem(ceres::Problem *problem) {
   CHECK(problem != NULL);
 
   ceres::Solver::Options options;
@@ -341,8 +260,8 @@ bool SolveOptimizationProblem(ceres::Problem* problem) {
 }
 
 // Output the poses to the file with format: ID x y yaw_radians.
-bool OutputPoses(const std::string& filename,
-                 const std::map<int, Pose2d>& poses) {
+bool OutputPoses(const std::string &filename,
+                 const std::map<int, Pose2d> &poses) {
   std::fstream outfile;
   outfile.open(filename.c_str(), std::istream::out);
   if (!outfile) {
@@ -351,18 +270,18 @@ bool OutputPoses(const std::string& filename,
   }
   for (std::map<int, Pose2d>::const_iterator poses_iter = poses.begin();
        poses_iter != poses.end(); ++poses_iter) {
-    const std::map<int, Pose2d>::value_type& pair = *poses_iter;
-    outfile <<  pair.first << " " << pair.second.x << " " << pair.second.y
-            << ' ' << pair.second.yaw_radians << '\n';
+    const std::map<int, Pose2d>::value_type &pair = *poses_iter;
+    outfile << pair.first << " " << pair.second.x << " " << pair.second.y << ' '
+            << pair.second.yaw_radians << '\n';
   }
   return true;
 }
 
-}  // namespace
-}  // namespace examples
-}  // namespace ceres
+} // namespace
+} // namespace examples
+} // namespace ceres
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
   GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
 
