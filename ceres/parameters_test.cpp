e27d98728b309526cd0fd287ca82ac0a386c69db
Frank Dellaert <dellaert@gmail.com>
2020-08-10 11:46:09 -0400

    Added tests for ParameterMap

--- ceres/parameters_test.cpp
+++ ceres/parameters_test.cpp
@@ -13,6 +13,7 @@
 #include "gtest/gtest.h"
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 using namespace shonan;
@@ -38,5 +39,26 @@ TEST(ParameterBuffer, SOn) {
     expected(0,0) = 2;
     M(0,0) = 2;
     SOn changed = parameters.At(0);
+    EXPECT_TRUE(expected.isApprox(changed.matrix()));
+}
+
+TEST(ParameterMap, SOn) {
+    // Create buffer and add SO(4) object
+    ceres::ParameterMap<string> parameters;
+    parameters.Insert("key", SOn(4));
+
+    // Check At
+    Matrix expected = Matrix::Identity(4,4);
+    EXPECT_TRUE(expected.isApprox(parameters.At<SOn>("key").matrix()));
+
+    // Check that we can access unsafely
+    double* unsafe = parameters.Unsafe("key");
+    Eigen::Map<Matrix> M(unsafe, 4, 4);
+    EXPECT_TRUE(expected.isApprox(M));
+
+    // and change
+    expected(0,0) = 2;
+    M(0,0) = 2;
+    SOn changed = parameters.At<SOn>("key");
     EXPECT_TRUE(expected.isApprox(changed.matrix()));
 }
